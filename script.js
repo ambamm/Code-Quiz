25e1b96a25164159668152de1a07ce56c7931ece
Amber <amberpitts@gmail.com>
2022-12-01 19:57:02 -0700

    more coding

---
+++ script.js
@@ -0,0 +1,29 @@
+var questions = [   //building object
+    {
+        question: "q1", //array of questions 
+        choices: [ "a1", "a2", "a3", "a4" ], 
+        correctAnswer: "a3",
+
+    },
+    {
+        question: "q2", 
+        choices: [ "a1", "a2", "a3", "a4" ], 
+        correctAnswer: "a3",
+
+    },
+    {
+        question: "q3", 
+        choices: [ "a1", "a2", "a3", "a4" ], 
+        correctAnswer: "a3",
+
+    },
+    {
+        question: "q4", 
+        choices: [ "a1", "a2", "a3", "a4" ], 
+        correctAnswer: "a3",
+
+    },
+
+
+
+]
