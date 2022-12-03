df2f68f15ce6d8fe4e65fe0cd4072ce1611df0e1
Amber <amberpitts@gmail.com>
2022-12-03 11:42:58 -0700

    changed arrays and index a bit

--- script.js
+++ script.js
@@ -5,31 +5,36 @@ var questions = [   //building object
                  quesB: "Sipping coffee while writing code",
                  quesC: "An object oriented program language",
                  quesD: "A backend library used to store tables",
+                 correctAnswer: "quesA"
 
     },
+    {
+    question: "What is CSS in programing", //array of questions 
+    choices: quesA: "Building Case Studies in programing";
+             quesB: "Cascading Sigma Styles",
+             quesC: "A backend language used to style the frontend layout",
+             quesD: "Styles the layout of an HTML page",
+             correctAnswer: "quesA"
+
     
-    ], 
-        correctAnswer: "a3",
-
-    
-    {
-        question: "What is Css?", 
-        choices: [ "a1", "a2", "a3", "a4" ], 
-        correctAnswer: "a3",
-
-    },
-    {
-        question: "HTML is known as what in computer programing", 
-        choices: [ "a1", "a2", "a3", "a4" ], 
-        correctAnswer: "a3",
-
-    },
-    {
-        question: "Visual Studio Code allows users to:", 
-        choices: [ "a1", "a2", "a3", "a4" ], 
-        correctAnswer: "a3",
-
     },
 
+    {
+        question: "A web API can do the following:", //array of questions 
+        choices: quesA: "Extend functionality of the browser",
+                 quesB: "Abolish Programing Interfaces",
+                 quesC: "Prevent cyber hackers from hacking",
+                 quesD: "Connect users to local 404 hosts",
+                 correctAnswer: "quesA"
+    },
+    {
+        question: "DOM methods are:", //array of questions 
+        choices: quesA: "Building dynamic pages",
+                 quesB: "Properties of CSS you perform in HTML",
+                 quesC: "Actions you can perform on HTML Elements",
+                 quesD: "An aspect of the terminal that Dominates the HTML in Javascript",
+                 correctAnswer: "quesA"
+    },
+];
 
 
