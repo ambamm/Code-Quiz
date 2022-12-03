68435e20f8a3b49231bd4e8303e271c3b6cebe03
Amber <amberpitts@gmail.com>
2022-12-03 11:01:58 -0700

    adding code

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 var questions = [   //building object
     {
-        question: "q1", //array of questions 
+        question: "What is JavaScript?", //array of questions 
+        choices: quesA: "Building dynamic pages",
+                 quesB: "Sipping coffee while writing code",
+                 quesC: "An object oriented program language",
+                 quesD: "A backend library used to store tables",
+
+    },
+    
+    ], 
+        correctAnswer: "a3",
+
+    
+    {
+        question: "What is Css?", 
         choices: [ "a1", "a2", "a3", "a4" ], 
         correctAnswer: "a3",
 
     },
     {
-        question: "q2", 
+        question: "HTML is known as what in computer programing", 
         choices: [ "a1", "a2", "a3", "a4" ], 
         correctAnswer: "a3",
 
     },
     {
-        question: "q3", 
-        choices: [ "a1", "a2", "a3", "a4" ], 
-        correctAnswer: "a3",
-
-    },
-    {
-        question: "q4", 
+        question: "Visual Studio Code allows users to:", 
         choices: [ "a1", "a2", "a3", "a4" ], 
         correctAnswer: "a3",
 
@@ -26,4 +33,3 @@ var questions = [   //building object
 
 
 
-]
