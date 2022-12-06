98ccac61db4f8eab89a71c749dc4dc09ea4e1538
Amber <amberpitts@gmail.com>
2022-12-05 19:58:45 -0700

    added more functions and variables

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ var questionEl = document.querySelector("#questions-div");
 var endEl = document.querySelector("#end-div");
 var submitEl = document.querySelector("#submit");
 var resultsEl = document.querySelector("results");
+var questionIndex = 0;
+var choicesEl = document.getElementById("choices");
+var intervalId;
+var time = 60;
 
 //increment and decrement functions?
 
@@ -15,13 +19,13 @@ var questions = [
   //array of objects
   {
     question: "What is JavaScript?", //objects
-    choices: {
-      quesA: "Building dynamic pages",
-      quesB: "Sipping coffee while writing code",
-      quesC: "An object oriented program language",
-      quesD: "A backend library used to store tables",
-    },
-    answer: "quesA",
+    choices: [
+      "Building dynamic pages",
+      "Sipping coffee while writing code",
+      "An object oriented program language",
+      "A backend library used to store tables",
+    ],
+    answer: "An object oriented program language",
   },
   {
     question: "What is CSS in programing?",
@@ -57,15 +61,55 @@ var questions = [
 
 // variables
 
-var intervalId;
-var time = 5;
-
 //eventlist
 function startQuiz() {
   startEl.classList.add("hide");
   questionEl.classList.remove("hide");
 
   intervalId = setInterval(tiktok, 1000);
+
+  //not writing, but planing to exec
+  getQuestion();
+}
+
+function getQuestion() {
+  var currentQuestion = questions[questionIndex];
+  var titleEl = document.getElementById("question");
+  titleEl.textContent = currentQuestion.question;
+  //sets html, anything inside as am empty string, nothing inside
+  choicesEl.innerHTML = "";
+
+  for (let index = 0; index < currentQuestion.choices.length; index++) {
+    var choice = currentQuestion.choices[index];
+    var choiceButton = document.createElement("button");
+    choiceButton.setAttribute("class", "choice");
+    choiceButton.setAttribute("value", choice);
+    choiceButton.textContent = index + 1 + "." + choice;
+    choicesEl.appendChild(choiceButton);
+  }
+}
+
+function choiceClick (event) {
+var buttonEl = event.target
+if (!buttonEl.matches(".choice")) {
+  return;
+}
+//if they're wrong
+ if (buttonEl.value !== questions[questionIndex].answer) {
+  time-=10
+  if (time<0) {
+    time = 0
+  }
+  timeEl.textContent=time;
+ } 
+ questionIndex++
+ if (time<=0 || questionIndex===questions.length) {
+  //end quiz function
+ }
+else {
+  getQuestion ()
+}
+
 }
 
 function tiktok() {
@@ -76,19 +120,14 @@ function tiktok() {
   }
 }
 
+function displayAnswer() {
+  var answerButton1 = document.createElement("button");
+  answerButton1.textContent = questions[questionsIndex].quesA;
 
-
-function displayAnswer () {
-    var answerButton1 = document.createElement("button");
-    answerButton1.textContent = questions[questionsIndex].quesA;
-
-    answer.appendChild(submitEl);
+  answer.appendChild(submitEl);
 }
 
-function showResults() { 
-
-
-}
+function showResults() {}
 
 function endGame() {
   clearInterval(intervalId);
@@ -96,25 +135,18 @@ function endGame() {
   endEl.classList.remove("hide");
 }
 
-
 //eventListeners start here - always at the bottom
 button.addEventListener("click", startQuiz);
 
-var answerButton1 = document.createElement("button"); 
+var answerButton1 = document.createElement("button");
 //it needs text content=answer1
 
-var questionIndex = 0;
-answerButton1.textContent = question [questionIndex].quesA;
-
-
-
+// var questionIndex = 0;
+// answerButton1.textContent = question [questionIndex].quesA;
 
 var answerButton2 = document.createElement("button");
 var answerButton3 = document.createElement("button");
 var answerButton4 = document.createElement("button");
-
-
-
 
 // Need functions:
 //q/a's
